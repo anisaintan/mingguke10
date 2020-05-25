0e75b0c5089a37958aa473410d4d4961ee7319e7
anisaintan <62274502+anisaintan@users.noreply.github.com>
2020-05-25 15:12:46 +0700

    kirim semua file

---
+++ menu.php
@@ -0,0 +1,8 @@
+<div class="menu">
+<ul>
+	<li><a href="index.php?p=home">SUBMIT</a></li>
+	<li><a href="edit.php?p=sekolah">EDIT</a></li>
+	<li><a href="records.php?p=sekolah">RECORDS</a></li>
+	<li><a href="read.php?p=sekolah">READ</a></li>
+</ul>
+</div>
